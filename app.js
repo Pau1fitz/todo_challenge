2e73d48c3477ea18d1d4371c3f31c1e64ed6b15d
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-21 20:48:18 +0000

    added styling and bootstrap

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ var toDoList = angular.module('toDoList',[]);
 
 toDoList.controller('toDoListController', function($scope){
   $scope.toDos = [];
-  $scope.tasks = 0
+  $scope.tasks = 0;
+  $scope.id = -1;
 
   $scope.submit = function(){
     if($scope.text){
-    $scope.toDos.push({text: this.text, done:false});
+    $scope.toDos.push({id: $scope.id += 1, text: this.text, done:false});
     $scope.text = '';
     $scope.tasks += 1
     };
   };
 
-  $scope.clearOne = function(){
-    $scope.toDos.done = true
+  $scope.clearOne = function(index){
     $scope.tasks -= 1;
+    $scope.toDos[index].done = true
     console.log($scope.toDos)
-
-
   };
 
   $scope.clear = function(){
