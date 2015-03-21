789c9c633543c0f9cd9b760ade4704961c915b8a
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-21 22:30:39 +0000

    set up protractor

---
+++ test/todolist-spec.js
@@ -0,0 +1,12 @@
+describe('toDoList homepage', function() {
+  it('should add a todo', function() {
+    browser.get('file://localhost/Users/paulfitzgerald/Desktop/Makers%20Academy/to_do_list_angular/app.html');
+
+    element(by.model('todoText')).sendKeys('write a protractor test');
+    element(by.css('[value="add"]')).click();
+
+    var todoList = element.all(by.repeater('todo in todos'));
+    expect(todoList.count()).toEqual(3);
+    expect(todoList.get(2).getText()).toEqual('write a protractor test');
+  });
+});
