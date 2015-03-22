a8eb5d5ca73fe770482ee531e72cdcf82dcf972b
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-22 12:34:21 +0000

    fully tested with protractor and jasmine unit tests

--- test/todolist-spec.js
+++ test/todolist-spec.js
@@ -20,4 +20,32 @@ describe('toDoList homepage', function() {
     element(by.id('button')).click();
     expect(element(by.id('list')).getText()).toContain("Go To School" && "Go Home");
   });
+
+    it('should show completed tasks filtered if completed button is clicked', function() {
+    browser.get('http://localhost:3000/app.html');
+    element(by.model('text')).sendKeys("Go To School");
+    element(by.id('button')).click();
+    element(by.id('checkbox')).click();
+    element(by.id('completeButton')).click();
+    expect(element(by.id('list')).getText()).toContain("Go To School");
+  });
+
+    it('should show incomplete tasks filtered if incomplete button is clicked', function() {
+    browser.get('http://localhost:3000/app.html');
+    element(by.model('text')).sendKeys("Go To School");
+    element(by.id('button')).click();
+    element(by.id('checkbox')).click();
+    element(by.model('text')).sendKeys("Go Home");
+    element(by.id('button')).click();
+    element(by.id('incompleteButton')).click();
+    expect(element(by.id('list')).getText()).toContain("Go Home");
+  });
+
+    it('should clear all tasks if clear button is clicked', function() {
+    browser.get('http://localhost:3000/app.html');
+    element(by.model('text')).sendKeys("Go To School");
+    element(by.id('button')).click();
+    element(by.id('clearButton')).click();
+    expect(element(by.id('list')).getText()).not.toContain("Go To School");
+  });
 });
